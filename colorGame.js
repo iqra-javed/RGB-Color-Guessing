808c95c2427e71b4473a65bc6c744f41f4ebd630
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2019-02-02 15:32:59 -0600

    add hard coded colors

---
+++ colorGame.js
@@ -0,0 +1,16 @@
+const colors = [
+  'rgb(255, 0, 0)',
+  'rgb(255, 255, 0)',
+  'rgb(0, 255, 0)',
+  'rgb(0, 255, 255)',
+  'rgb(0, 0, 255)',
+  'rgb(255, 0, 255)'
+];
+
+const squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+squares.forEach((color, index) => color.style.backgroundColor = colors[index])
