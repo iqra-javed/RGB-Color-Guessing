bfb9dcefe0c9aa0b5eaae710823a4eb0870ec62f
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2019-02-02 19:50:41 -0600

    add easy / hard modes

--- colorGame.js
+++ colorGame.js
@@ -1,25 +1,57 @@
-let colors = generateRandomColors(6);
-
+let numOfSquares = 6;
+let colors = generateRandomColors(numOfSquares);
 const squares = document.querySelectorAll('.square');
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
 let messageDisplay = document.querySelector('#message');
-let h1 = document.querySelector('h1');
+const h1 = document.querySelector('h1');
 let resetButton = document.querySelector('#reset');
+const eastBtn = document.querySelector('#easyBtn');
+const hardBtn = document.querySelector('#hardBtn');
+
+easyBtn.addEventListener('click', function() {
+  easyBtn.classList.add('selected');
+  hardBtn.classList.remove('selected');
+  numOfSquares = 3;
+  colors = generateRandomColors(numOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  squares.forEach((square, index) => {
+    if (colors[index]) {
+      square.style.background = colors[index];
+    } else {
+      square.style.display = 'none';
+    }
+  });
+});
+
+hardBtn.addEventListener('click', function() {
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+  numOfSquares = 6;
+  colors = generateRandomColors(numOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  squares.forEach((square, index) => {
+    square.style.background = colors[index];
+    square.style.display = 'block';
+  });
+});
 
 resetButton.addEventListener('click', function() {
-
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numOfSquares);
 
   pickedColor = pickColor();
 
-  // change colorDisplay to match pickedColor
   colorDisplay.textContent = pickedColor;
 
-    squares.forEach((square, index) => {
-        square.style.backgroundColor = colors[index];
-    });
-    h1.style.backgroundColor = '#232323';
+  squares.forEach((square, index) => {
+    square.style.backgroundColor = colors[index];
+  });
+  h1.style.backgroundColor = '#232323';
+  resetButton.textContent = 'New Colors';
 });
 
 colorDisplay.textContent = pickedColor;
